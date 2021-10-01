54e79e7b1a45de4c3b5d05f18782282f630dc83a
“jungkeunmo“ <abc59684495@gmail.com>
2021-10-01 21:49:04 +0900

    spa

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+const globalRouter = require("./routers/globalRouter");
+const path = require("path");
+
+const PORT = 4000;
+const app = express();
+
+app.set(`view engine`, `pug`);
+app.use(morgan(`dev`));
+app.use(express.static(path.join(__dirname, "/assets")));
+//static = 정적 자원
+
+app.use("/", globalRouter);
+
+app.listen(PORT, () => {
+    console.log(`${PORT} WEB SERVER START!🥕`);
+});
