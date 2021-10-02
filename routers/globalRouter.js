e8e7f210a0b45369832709158daaf3cab7deb65a
“jungkeunmo“ <abc59684495@gmail.com>
2021-10-02 14:54:20 +0900

    spq

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -6,4 +6,8 @@ router.get("/", (req, res, next) => {
     res.render("screens/main");
 });
 
+router.get("/review", (req, res, next) => {
+    res.render("screens/review");
+});
+
 module.exports = router;
