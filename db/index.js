bfb3a882276e24ae1ffabff7570a5e78a5034017
“jungkeunmo“ <abc59684495@gmail.com>
2021-10-08 21:12:59 +0900

    spa

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const db = mysql2.createPool({
     port : "3306",
     database : "camille_db",
     user : "root",
-    password : "fourleaf0309!!",
+    password : "fourleaf0309",
 });
 
 module.exports = db;
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -7,22 +7,60 @@ router.get("/", (req, res, next) => {
     res.render("screens/main");
 });
 
-router.get("/review", (req, res, next) => {
+router.get("/review", async (req, res, next) => {
+
+    let result1 = null;
+    let result2 = null;
+
+    //데이터가오기
     const query1 = `
     SELECT	COUNT(id)	AS count,
             AVG(score)	AS avg
-      FROM	reviews;
+      FROM	reviews
     `;
     const query2 = `
     SELECT	id,
             score,
             content
       FROM	reviews
-     ORDER	BY	id	DESC;
+     ORDER	BY	score	DESC
     `;
-    //데이터가오기
-    console.log(db);
-    res.render("screens/review");
+
+    try {
+        db.query(query1, (error, result) => {
+            if(error) {
+                return console.error(error);
+            }
+            result1 = result;
+
+            db.query(query2, (error, result) => {
+                if(error) {
+                    return console.error(error);
+                }
+                result2 = result;
+
+                console.log(result1[0]);
+                console.log(result2);
+
+                res.render("screens/review", {
+                    result1:result1[0],
+                    result2:result2,
+                });
+            });
+        });
+
+        //db.query(query2, (error, result) => {
+            //if(error) {
+                //return console.error(error);
+            //}
+            //result2 = result;
+        //});
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
 });
 
 module.exports = router;
